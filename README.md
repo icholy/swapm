c604b706956633bb443f08a70865c25912bd0469
Ilia Choly <icholy@accipiterradar.com>
2012-10-17 18:18:44 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Swap
+
+> Code generation for the rest of us
+
+## Example
+
+**Source File:** `project/foo.h`
+
+    #ifndef FOO_H_
+    #define FOO_H_
+
+    #include <libpq-fe.h>
+    #include <libpqtypes.h>
+
+    class Foo {
+    private:
+      /*{ template: "t1", data: "d1" }*/
+
+                          <-- generated code will go here
+
+      /*{end}*/
+    public:
+      Foo(PGresult * res);
+      virtual ~Foo();
+    };
+
+    #endif /* FOO_H_ */
+
+**Data File:** `project/swap/data/d1.js`
+
+    module.exports = {
+      stuff: [
+        { name: "id", type: "int8"  },
+        { name: "bar",type: "float4"}
+      ]
+    }
+
+**Template File:** `project/swap/templates/t1.tmpl`
+
+* Mustache templates are used by default, but the engine will be configurable :)
+
+    {{#stuff}}
+      PG{{type}} * _{{name}};
+    {{/stuff}}
+
+## Generate
+
+    swap foo.h
+
+## Output
+
+    #ifndef FOO_H_
+    #define FOO_H_
+
+    #include <libpq-fe.h>
+    #include <libpqtypes.h>
+
+    class Foo {
+    private:
+      /*{ template: "t1", data: "d1" }*/
+      PGint8 * _id;
+      PGfloat4 * _bar;
+      /*{end}*/
+    public:
+      Foo(PGresult * res);
+      virtual ~Foo();
+    };
+
+    #endif /* FOO_H_ */
+
+
+
+
+
---
+++ swap.js
@@ -0,0 +1,153 @@
+
+var mustache = require('mustache');
+var fs       = require('fs');
+var walk     = require('walk');
+var path     = require('path');
+
+module.exports = (function(){
+
+  var templates = {};
+  var dataSets  = {};
+
+  var render = function(tmpl, data) {
+    return mustache.render(tmpl, data);
+  };
+
+  var inject = function(tmplName, dataName, src, start, end) {
+
+      var tmpl = templates[tmplName];
+      var data = dataSets[dataName];
+
+      if (!tmpl)    throw tmplName + " template not found";
+      if (!dataName) throw dataName + " data not found";
+
+      return src.substring(0, start)
+           + render(tmpl, data)
+           + src.substring(end, src.length);
+  };
+
+  var findInjectionPoints = function(src) {
+    
+    var idx     = 0,
+        pat     = /\/\*{([^}]*)}\*\//g,
+        points  = [];
+
+    while (true) {
+
+      var start = pat.exec(src),
+          end   = pat.exec(src),
+          params;
+
+      if (!start) break;
+      if (!end)   throw "open/close mismatch error";
+
+      var startContent = start[1].trim(),
+          endContent = end[1].trim();
+
+      if (startContent == "end" || endContent != "end" || start.index > end.index)
+        throw "open/close order error";
+
+      try {
+        params = eval('({'+startContent+'})');
+      } catch (e) {
+        throw "invalid open syntax";
+      }
+
+      if (!params)          throw "invalid open syntax";
+      if (!params.data)     throw "missing data parameter";
+      if (!params.template) throw "missing template parameter";
+
+      idx = end.index;
+
+      points.push({
+        start:    start.index + start[0].length,
+        end:      end.index,
+        template: params.template,
+        data:     params.data,
+        params:   params
+      });
+    }
+
+    return points;
+  };
+
+  var loadTemplates = function(dir) {
+    walk.walk(dir).on("file", function(root, stat, next) {
+
+      var fpath = path.join(dir, stat.name);
+      var ext   = path.extname(fpath);
+      var name  = path.basename(fpath, ext);
+
+      fs.readFile(fpath, 'utf-8', function(err, data) {
+        if (!err) {
+          templates[name] = data;
+        } else {
+          console.log('failed to read' + fpath);
+        }
+      });
+      next();
+    });
+  };
+
+  var loadData = function(dir) {
+    walk.walk(dir).on("file", function(root, stat, next) {
+
+      var ext   = path.extname(stat.name);
+      var name  = path.basename(stat.name, ext);
+      var fpath = path.join(dir, name);
+      if (ext == '.js') {
+        dataSets[name] = require(fpath);
+      }
+      next();
+    });
+  };
+
+  return {
+
+    addTemplate: function(name, template) {
+      templates[name] = template;
+    },
+
+    addData: function(name, data) {
+      dataSets[name] = data;
+    },
+
+    getDataNames: function() {
+      return Object.keys(dataSets);
+    },
+
+    getTemplateNames: function() {
+      return Object.keys(templates);
+    },
+
+    load: function(dir) {
+      dir = path.join(dir || process.cwd(), "swap");
+      loadTemplates(path.join(dir, 'templates'));
+      loadData(path.join(dir, 'data'));
+    },
+
+    process: function(src) {
+       
+      findInjectionPoints(src).forEach(function(point) {
+        src = inject(
+          point.template,
+          point.data,
+          src,
+          point.start,
+          point.end
+        );
+      });
+
+      return src;
+    },
+
+    processFile: function(fname, fn) {
+      var _this = this;
+      fs.readFile(fname, 'utf-8', function(err, src) {
+        if (!err) fn(_this.process(src));
+        else throw "error reading " + fname;
+      });
+    }
+  };
+
+}());
